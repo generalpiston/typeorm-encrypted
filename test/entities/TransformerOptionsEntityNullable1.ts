57bc7b52f6a86c6d99607d9a73df13f852329f17
Abraham Elmahrek <abraham@elmahrek.com>
2020-10-02 12:04:51 -0700

    #27: Add tests for null and undefined values for transformer

---
+++ test/entities/TransformerOptionsEntityNullable1.ts
@@ -0,0 +1,20 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { EncryptionTransformer } from '../../src/transformer';
+
+@Entity()
+export default class TransformerOptionsEntityNullable1 {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    type: 'varchar',
+    nullable: true,
+    transformer: new EncryptionTransformer({
+      key: 'e41c966f21f9e1577802463f8924e6a3fe3e9751f201304213b2f845d8841d61',
+      algorithm: 'aes-256-cbc',
+      ivLength: 16,
+      iv: 'ff5ac19190424b1d88f9419ef949ae56'
+    })
+  })
+  secret: string|null;
+}
--- test/transformer.test.ts
+++ test/transformer.test.ts
@@ -2,6 +2,8 @@ import { expect } from 'chai';
 import { Connection } from 'typeorm';
 import { withConnection } from './utils';
 import TransformerOptionsEntity1 from './entities/TransformerOptionsEntity1';
+import TransformerOptionsEntityNullable1 from './entities/TransformerOptionsEntityNullable1';
+import TransformerOptionsEntityNullable2 from './entities/TransformerOptionsEntityNullable2';
 import TransformerOptionsAES256GCMEntity1 from './entities/TransformerOptionsAES256GCMEntity1';
 import TransformerOptionsAES256GCMEntity2 from './entities/TransformerOptionsAES256GCMEntity2';
 
@@ -50,8 +52,8 @@ describe('Transformer', () => {
     });
   });
 
-  it('should automatically encrypt and decrypt aes-256-gcm with 8-byte long auth tag length', async () => {
-    await withConnection(async (connection: Connection) => {
+  it('should automatically encrypt and decrypt aes-256-gcm with 8-byte long auth tag length', (done) => {
+    withConnection(async (connection: Connection) => {
       const manager = connection.manager;
       const repo = connection.getRepository(TransformerOptionsAES256GCMEntity2);
       const instance = await repo.create({ secret: 'test' });
@@ -69,6 +71,58 @@ describe('Transformer', () => {
 
       expect(results.length).to.equal(1);
       expect(results[0].secret).to.equal('test');
+
+      done();
+    });
+  });
+
+  it('should not encrypt / decrypt null values', (done) => {
+    withConnection(async (connection: Connection) => {
+      const manager = connection.manager;
+      const repo = connection.getRepository(TransformerOptionsEntityNullable1);
+      const instance = await repo.create({ secret: null });
+      await repo.save(instance);
+
+      const result = await manager.query(
+        'SELECT secret FROM transformer_options_entity_nullable1'
+      );
+      
+
+      expect(result[0].secret).to.equal(
+        null
+      );
+
+      const results = await repo.find();
+
+      expect(results.length).to.equal(1);
+      expect(results[0].secret).to.equal(undefined);
+
+      done()
+    });
+  });
+
+  it('should not encrypt / decrypt undefined values', (done) => {
+    withConnection(async (connection: Connection) => {
+      const manager = connection.manager;
+      const repo = connection.getRepository(TransformerOptionsEntityNullable2);
+      const instance = await repo.create({ secret: undefined });
+      await repo.save(instance);
+
+      const result = await manager.query(
+        'SELECT secret FROM transformer_options_entity_nullable2'
+      );
+      
+
+      expect(result[0].secret).to.equal(
+        null
+      );
+
+      const results = await repo.find();
+
+      expect(results.length).to.equal(1);
+      expect(results[0].secret).to.equal(undefined);
+
+      done()
     });
   });
 });
