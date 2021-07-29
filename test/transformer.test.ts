c3119c043a9b4d1796544196b397d5f1ed4f6488
Abraham Elmahrek <abraham@elmahrek.com>
2021-07-29 08:48:06 -0700

    #41: Add test case for querybuilder w/ transformers

--- test/transformer.test.ts
+++ test/transformer.test.ts
@@ -1,117 +1,117 @@
-import { expect } from 'chai';
-import { Connection } from 'typeorm';
-import { getConnection } from './utils';
-import TransformerOptionsEntityEmptyString1 from './entities/TransformerOptionsEntityEmptyString1';
-import TransformerOptionsEntity1 from './entities/TransformerOptionsEntity1';
-import TransformerOptionsEntityNullable1 from './entities/TransformerOptionsEntityNullable1';
-import TransformerOptionsEntityNullable2 from './entities/TransformerOptionsEntityNullable2';
-import TransformerOptionsAES256GCMEntity1 from './entities/TransformerOptionsAES256GCMEntity1';
-import TransformerOptionsAES256GCMEntity2 from './entities/TransformerOptionsAES256GCMEntity2';
+import { expect } from "chai";
+import { Connection } from "typeorm";
+import { getConnection } from "./utils";
+import TransformerOptionsEntityEmptyString1 from "./entities/TransformerOptionsEntityEmptyString1";
+import TransformerOptionsEntity1 from "./entities/TransformerOptionsEntity1";
+import TransformerOptionsEntityNullable1 from "./entities/TransformerOptionsEntityNullable1";
+import TransformerOptionsEntityNullable2 from "./entities/TransformerOptionsEntityNullable2";
+import TransformerOptionsAES256GCMEntity1 from "./entities/TransformerOptionsAES256GCMEntity1";
+import TransformerOptionsAES256GCMEntity2 from "./entities/TransformerOptionsAES256GCMEntity2";
 
-describe('Transformer', function() {
+describe("Transformer", function () {
   let connection: Connection;
 
   this.timeout(10000);
 
   before(async function () {
     connection = await getConnection();
-  })
+  });
 
-  it('should automatically encrypt and decrypt', async function() {
+  it("should automatically encrypt and decrypt", async function () {
     const manager = connection.manager;
     const repo = connection.getRepository(TransformerOptionsEntity1);
-    const instance = await repo.create({ secret: 'test' });
-    await repo.save(instance);
+    const instance = await repo.create({ secret: "test" });
 
-    const result = await manager.query(
-      'SELECT secret FROM transformer_options_entity1'
-    );
+    try {
+      await repo.save(instance);
 
-    expect(result[0].secret).to.equal(
-      '/1rBkZBCSx2I+UGe+UmuVhKzmHsDDv0EvRtMBFiaE3A='
-    );
+      const result = await manager.query("SELECT secret FROM transformer_options_entity1");
 
-    const results = await repo.find();
+      expect(result[0].secret).to.equal("/1rBkZBCSx2I+UGe+UmuVhKzmHsDDv0EvRtMBFiaE3A=");
 
-    expect(results.length).to.equal(1);
-    expect(results[0].secret).to.equal('test');
+      const results = await repo.find();
+
+      expect(results.length).to.equal(1);
+      expect(results[0].secret).to.equal("test");
+    } finally {
+      await repo.clear();
+    }
   });
 
-  it('should automatically encrypt and decrypt aes-256-gcm', async function() {
+  it("should automatically encrypt and decrypt aes-256-gcm", async function () {
     const manager = connection.manager;
     const repo = connection.getRepository(TransformerOptionsAES256GCMEntity1);
-    const instance = await repo.create({ secret: 'test' });
-    await repo.save(instance);
+    const instance = await repo.create({ secret: "test" });
 
-    const result = await manager.query(
-      'SELECT secret FROM transformer_options_entity1'
-    );
+    try {
+      await repo.save(instance);
 
-    expect(result[0].secret).to.equal(
-      '/1rBkZBCSx2I+UGe+UmuVhKzmHsDDv0EvRtMBFiaE3A='
-    );
+      const result = await manager.query("SELECT secret FROM transformer_options_aes256gcm_entity1");
 
-    const results = await repo.find();
+      expect(result[0].secret).to.equal("/1rBkZBCSx2I+UGe+UmuVpb6nX/euiab5zJklG0vyFvOSkuV");
 
-    expect(results.length).to.equal(1);
-    expect(results[0].secret).to.equal('test');
+      const results = await repo.find();
+
+      expect(results.length).to.equal(1);
+      expect(results[0].secret).to.equal("test");
+    } finally {
+      await repo.clear();
+    }
   });
 
-  it('should automatically encrypt and decrypt aes-256-gcm with 8-byte long auth tag length', async function() {
+  it("should automatically encrypt and decrypt aes-256-gcm with 8-byte long auth tag length", async function () {
     const manager = connection.manager;
     const repo = connection.getRepository(TransformerOptionsAES256GCMEntity2);
-    const instance = await repo.create({ secret: 'test' });
-    await repo.save(instance);
+    const instance = await repo.create({ secret: "test" });
 
-    const result = await manager.query(
-      'SELECT secret FROM transformer_options_entity1'
-    );
+    try {
+      await repo.save(instance);
 
-    expect(result[0].secret).to.equal(
-      '/1rBkZBCSx2I+UGe+UmuVhKzmHsDDv0EvRtMBFiaE3A='
-    );
+      // console.log(await manager.query("SELECT name FROM sqlite_master WHERE type='table'"));
 
-    const results = await repo.find();
+      const result = await manager.query("SELECT secret FROM transformer_options_aes256gcm_entity2");
 
-    expect(results.length).to.equal(1);
-    expect(results[0].secret).to.equal('test');
+      expect(result[0].secret).to.equal("/1rBkZBCSx2I+UGe+UmuVpb6nX/euiabzkpLlQ==");
+
+      const results = await repo.find();
+
+      expect(results.length).to.equal(1);
+      expect(results[0].secret).to.equal("test");
+    } finally {
+      await repo.clear();
+    }
   });
 
-  it('should not encrypt / decrypt null values', async function() {
+  it("should not encrypt / decrypt null values", async function () {
     const manager = connection.manager;
     const repo = connection.getRepository(TransformerOptionsEntityNullable1);
     const instance = await repo.create({ secret: null });
 
-    await repo.save(instance);
+    try {
+      await repo.save(instance);
 
-    const result = await manager.query(
-      'SELECT secret FROM transformer_options_entity_nullable1'
-    );
+      const result = await manager.query("SELECT secret FROM transformer_options_entity_nullable1");
 
-    expect(result[0].secret).to.equal(
-      null
-    );
+      expect(result[0].secret).to.equal(null);
 
-    const results = await repo.find();
+      const results = await repo.find();
 
-    expect(results.length).to.equal(1);
-    expect(results[0].secret).to.equal(undefined);
+      expect(results.length).to.equal(1);
+      expect(results[0].secret).to.equal(undefined);
+    } finally {
+      await repo.clear();
+    }
   });
 
-  it('should not encrypt / decrypt undefined values', async function() {
+  it("should not encrypt / decrypt undefined values", async function () {
     const manager = connection.manager;
     const repo = connection.getRepository(TransformerOptionsEntityNullable2);
     const instance = await repo.create({ secret: undefined });
     await repo.save(instance);
 
-    const result = await manager.query(
-      'SELECT secret FROM transformer_options_entity_nullable2'
-    );
-    
+    const result = await manager.query("SELECT secret FROM transformer_options_entity_nullable2");
 
-    expect(result[0].secret).to.equal(
-      null
-    );
+    expect(result[0].secret).to.equal(null);
 
     const results = await repo.find();
 
@@ -119,24 +119,44 @@ describe('Transformer', function() {
     expect(results[0].secret).to.equal(undefined);
   });
 
-  it('should encrypt / decrypt empty strings', async function() {
+  it("should encrypt / decrypt empty strings", async function () {
     const manager = connection.manager;
     const repo = connection.getRepository(TransformerOptionsEntityEmptyString1);
-    const instance = await repo.create({ secret: '' });
-    await repo.save(instance);
+    const instance = await repo.create({ secret: "" });
 
-    const result = await manager.query(
-      'SELECT secret FROM transformer_options_entity_empty_string1'
-    );
-    
+    try {
+      await repo.save(instance);
 
-    expect(result[0].secret).to.equal(
-      '/1rBkZBCSx2I+UGe+UmuVvnjveB6onZ3uoKZCOZfzbk='
-    );
+      const result = await manager.query("SELECT secret FROM transformer_options_entity_empty_string1");
 
-    const results = await repo.find();
+      expect(result[0].secret).to.equal("/1rBkZBCSx2I+UGe+UmuVvnjveB6onZ3uoKZCOZfzbk=");
 
-    expect(results.length).to.equal(1);
-    expect(results[0].secret).to.equal('');
+      const results = await repo.find();
+
+      expect(results.length).to.equal(1);
+      expect(results[0].secret).to.equal("");
+    } finally {
+      await repo.clear();
+    }
+  });
+
+  it("should decrypt in QueryBuilder", async function () {
+    const manager = connection.manager;
+    const repo = connection.getRepository(TransformerOptionsEntity1);
+    const instance = await repo.create({ secret: "QueryBuilder" });
+
+    try {
+      await repo.save(instance);
+
+      const result = await manager.query("SELECT secret FROM transformer_options_entity1");
+
+      expect(result[0].secret).to.equal("/1rBkZBCSx2I+UGe+UmuVoscCrVZPUm9+v40quDkL+0=");
+
+      const results = await repo.createQueryBuilder("query").getMany();
+
+      expect(results[0].secret).to.equal("QueryBuilder");
+    } finally {
+      await repo.clear();
+    }
   });
 });
